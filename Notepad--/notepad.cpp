dc2b73ff5e070cfc5b3f05da485102f29d93a170
Ohad Shulz <oshultz@paloaltonetworks.com>
2017-06-20 13:57:19 +0300

    frist

---
+++ Notepad--/notepad.cpp
@@ -0,0 +1,19 @@
+#include "notepad.h"
+#include "ui_notepad.h"
+
+Notepad::Notepad(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::Notepad)
+{
+    ui->setupUi(this);
+}
+
+Notepad::~Notepad()
+{
+    delete ui;
+}
+
+void Notepad::on_quitButton_clicked()
+{
+    QCoreApplication::quit();
+}
---
+++ Notepad--/notepad.h
@@ -0,0 +1,25 @@
+#ifndef NOTEPAD_H
+#define NOTEPAD_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class Notepad;
+}
+
+class Notepad : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit Notepad(QWidget *parent = 0);
+    ~Notepad();
+
+private slots:
+    void on_quitButton_clicked();
+
+private:
+    Ui::Notepad *ui;
+};
+
+#endif // NOTEPAD_H
